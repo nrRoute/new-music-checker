20c95d30d6e5219e8dad7abf5c3da54dd0086440
nrRoute <kaleidoscope9784@gmail.com>
2025-04-04 05:54:24 +0900

    update: dump to json

--- main.py
+++ main.py
@@ -34,11 +34,13 @@ class Artist:
 class Release:
     def __init__(self,
         id: str,
+        artist_name: str,
         spotify_url: str,
         release_date: str,
         release_type: str = None,
     ):
         self.id = id
+        self.artist_name = artist_name
         self.spotify_url = spotify_url
         self.release_date = release_date
         self.release_type = release_type
@@ -61,14 +63,61 @@ def get_following_artists(sp: spotipy.Spotify) -> list[Artist]:
         artists.append(Artist(artist['id'], artist['name'], artist['external_urls']['spotify']))
     return artists
 
+def save_releases_to_file(releases: list[Release]):
+    releases_record = []
+    for release in releases:
+        releases_record.append({
+            'id': release.id,
+            'artist_name': release.artist_name,
+            'spotify_url': release.spotify_url,
+            'release_date': release.release_date,
+            'release_type': release.release_type
+        })
+    with open('releases.json', 'w') as file:
+        json.dump(releases_record, file)
+    logging.info("Releases saved to releases.json")
+
+def add_new_releases_to_file(releases: list[Release]):
+    releases_record = load_releases_from_file()
+    for release in releases:
+        releases_record.append(release)
+    save_releases_to_file(releases_record)
+
+def load_releases_from_file() -> list[Release]:
+    if not os.path.exists('releases.json'):
+        logging.info("No releases.json file found.")
+        return []
+
+    with open('releases.json', 'r') as file:
+        releases_record = json.load(file)
+
+    releases = []
+    for record in releases_record:
+        releases.append(Release(
+            id=record['id'],
+            artist_name=record['artist_name'],
+            spotify_url=record['spotify_url'],
+            release_date=record['release_date'],
+            release_type=record['release_type']
+        ))
+    return releases
+
+def contains_release(releases_record: list[Release], release: Release) -> bool:
+    for record in releases_record:
+        if record.id == release.id:
+            return True
+    return False
+
+def check_if_new_release(releases_record: list[Release], release: Release) -> bool:
+    return not contains_release(releases_record, release)
+
 def get_following_artists_new_releases(sp: spotipy.Spotify) -> list:
     artists = get_following_artists(sp)
-    artist_ids = [artist.id for artist in artists]
-    if not artist_ids:
+    if not artists:
         return []
     new_releases = []
-    for artist_id in artist_ids:
-        latest_releases = sp.artist_albums(artist_id, include_groups='album,single,appears_on', limit=5)
+    for artist in artists:
+        latest_releases = sp.artist_albums(artist.id, include_groups='album,single,appears_on', limit=5)
         if not latest_releases['items']:
             continue
 
@@ -78,6 +127,7 @@ def get_following_artists_new_releases(sp: spotipy.Spotify) -> list:
             if release_date >= today:
                 release = Release(
                     id=album['id'],
+                    artist_name=artist.name,
                     spotify_url=album['external_urls']['spotify'],
                     release_date=release_date,
                     release_type=album['album_type']
@@ -99,7 +149,7 @@ def push_message_to_discord(webhook_url: str, releases: list[Release]):
     }
     for release in releases:
         data = {
-            "content": f"New release: {release.release_date} - {release.spotify_url}"
+            "content": f"New release from {release.artist_name}: {release.release_date} - {release.spotify_url}"
         }
         response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
         if response.status_code != 204:
@@ -115,14 +165,22 @@ def main():
         logging.info("No followed artists found.")
         return
 
-    spotify_new_releases = get_following_artists_new_releases(sp)
+    spotify_releases = get_following_artists_new_releases(sp)
 
-    if not spotify_new_releases:
-        logging.info("No new releases found.")
+    if not spotify_releases:
+        logging.info("No recent releases found.")
         return
 
-    logging.info(f"Found {len(spotify_new_releases)} new releases.")
-    send_message_to_discord(spotify_new_releases)
+    releases_record = load_releases_from_file()
+    new_releases = []
+    for release in spotify_releases:
+        if not check_if_new_release(releases_record=releases_record, release=release):
+            continue
+        new_releases.append(release)
+
+    logging.info(f"Found {len(new_releases)} new releases.")
+    send_message_to_discord(new_releases)
+    add_new_releases_to_file(new_releases)
 
 
 if __name__ == "__main__":
